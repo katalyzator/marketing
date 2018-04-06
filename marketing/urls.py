c84de07b7f00d83108800b82c588c80577ff59ef
katalyzator <web.coder96@gmail.com>
2018-04-06 13:49:18 +0600

    init commit

---
+++ marketing/urls.py
@@ -0,0 +1,13 @@
+
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+
+from marketing import settings
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
