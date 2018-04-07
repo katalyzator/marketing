aec82d54b671d9d3fb7b9e010cafe48ee977f0aa
katalyzator <web.coder96@gmail.com>
2018-04-07 14:01:14 +0600

    added new model

--- main/admin.py
+++ main/admin.py
@@ -4,5 +4,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from main.models import Slider
 
 admin.site.site_header = 'Панель управления'
+
+
+class SliderAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Slider
+
+
+admin.site.register(Slider, SliderAdmin)
--- main/models.py
+++ main/models.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import smart_unicode
 
-# Create your models here.
+
+class Slider(models.Model):
+    title = models.CharField(max_length=500, verbose_name='Заголовок')
+    description = models.TextField(max_length=400, verbose_name='Описание')
+    image = models.ImageField(upload_to='images/slider', verbose_name='Картинка')
+
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    class Meta:
+        verbose_name = 'Слайдер'
+        verbose_name_plural = 'Раздел слайдов'
+
+    def __unicode__(self):
+        return smart_unicode(self.title)
+
+
