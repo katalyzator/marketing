bac4d2c9b7bd6752695dd5ce5c0946c774a6ea69
Noors <jackmovies01@gmail.com>
2018-04-28 01:55:19 +0600

    user ref fixed

--- main/views.py
+++ main/views.py
@@ -45,7 +45,7 @@ class UserCreateView(CreateView):
         if self.request.session['ref']:
             ref_user = User.objects.get(username=self.request.session['ref'])
         else:
-            ref_user = User.objects.get(username='admin6')
+            ref_user = User.objects.get(pk=15)
         user = form.save(commit=False)
         user.set_password(form.cleaned_data['password1'])
         user.is_active = False
