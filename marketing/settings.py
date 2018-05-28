932194b742a6154e92faa59518a31ae646a5ffdc
Noors <jackmovies01@gmail.com>
2018-05-28 17:47:24 +0600

    mail changed

--- marketing/settings.py
+++ marketing/settings.py
@@ -122,12 +122,12 @@ STATICFILES_DIRS = (
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.mail.ru'
 
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = 'wowtsty@gmail.com'
-EMAIL_HOST_PASSWORD = '124358911'
+EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = 'new_life_kg@mail.ru'
+EMAIL_HOST_PASSWORD = '1311maa_2018'
 
 JET_DEFAULT_THEME = 'default'
 
