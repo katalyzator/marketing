1956cc035e0aa57e36742586df3206f1aec35e7d
Nurs <jackmovies01@gmail.com>
2018-09-21 02:55:34 +0600

    2018-09-21 02:55:34.131411

--- marketing/settings.py
+++ marketing/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = '6cv&3uf^7wiu$%c8$xfjdpw^0^_#6f&h-8h@ceh%(r5ammuz=r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ON_WORK = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
