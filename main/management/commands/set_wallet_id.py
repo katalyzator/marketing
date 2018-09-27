c192f2aa73f7684ebae0e55f7a38ba6707fad576
Nurs <jackmovies01@gmail.com>
2018-09-27 22:08:59 +0600

    2018-09-27 22:08:59.851486

--- main/management/commands/set_wallet_id.py
+++ main/management/commands/set_wallet_id.py
@@ -10,5 +10,7 @@ from main.models import TransactionKeys, User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for user in User.objects.filter(wallet_id__isnull=True):
+            user.wallet_id = None
+            user.save()
             user.wallet_id = ''.join(random.choice(string.digits) for _ in range(7))
             user.save()
