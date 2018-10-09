cbc5c3892560cc106bfc42dde4864ae661564e27
Nurs <jackmovies01@gmail.com>
2018-10-09 23:34:34 +0600

    2018-10-09

--- main/dashboard.py
+++ main/dashboard.py
@@ -1,9 +1,15 @@
 # coding=utf-8
-from jet.dashboard.modules import DashboardModule
+from django.utils.translation import ugettext_lazy as _
+from jet.dashboard.dashboard import Dashboard
+
+from .modules import *
+from jet.dashboard.modules import *
 
 
-class BonusesCount(DashboardModule):
-    title = 'Бонусов в обороте'
-    title_url = ''
-    template = 'partials/bonus.html'
-    limit = 10
+class CustomDashboard(Dashboard):
+    columns = 4
+
+    def init_with_context(self, context):
+        # pass
+        self.available_children.append(BonusesQuantity(_(u'Сумма бонусов'), ))
+        self.children.append(BonusesQuantity(_(u'Сумма бонусов'), ))
---
+++ main/modules.py
@@ -0,0 +1,67 @@
+# coding=utf-8
+from django.db.models import Sum
+from django.utils.translation import ugettext_lazy as _
+
+from jet.dashboard.modules import DashboardModule
+from jet.utils import context_to_dict
+
+from main.models import User
+
+
+class BonusesQuantity(DashboardModule):
+    """
+    List of links widget.
+
+    Usage example:
+
+    .. code-block:: python
+
+        from django.utils.translation import ugettext_lazy as _
+        from jet.dashboard import modules
+        from jet.dashboard.dashboard import Dashboard, AppIndexDashboard
+
+
+        class CustomIndexDashboard(Dashboard):
+            columns = 3
+
+            def init_with_context(self, context):
+                self.available_children.append(modules.LinkList)
+                self.children.append(modules.LinkList(
+                    _('Support'),
+                    children=[
+                        {
+                            'title': _('Django documentation'),
+                            'url': 'http://docs.djangoproject.com/',
+                            'external': True,
+                        },
+                        {
+                            'title': _('Django "django-users" mailing list'),
+                            'url': 'http://groups.google.com/group/django-users',
+                            'external': True,
+                        },
+                        {
+                            'title': _('Django irc channel'),
+                            'url': 'irc://irc.freenode.net/django',
+                            'external': True,
+                        },
+                    ],
+                    column=0,
+                    order=0
+                ))
+
+    """
+
+    title = _('Сумма бонусов')
+    template = 'partials/bonus.html'
+    # model =
+    #: Specify widget layout.
+    #: Allowed values ``stacked`` and ``inline``.
+    layout = 'stacked'
+
+    def get_context_data(self):
+        context = context_to_dict(self.context)
+        context.update({
+            'module': self,
+            'bonuses': User.objects.aggregate(sum=Sum('points'))['sum'],
+        })
+        return context
