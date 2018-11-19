f1c933cd732f1fff8f727ff1600d5be25722104f
Nurs <jackmovies01@gmail.com>
2018-11-20 02:53:34 +0600

    2018-11-20

--- main/admin.py
+++ main/admin.py
@@ -11,7 +11,7 @@ admin.site.site_header = 'Панель управления'
 
 
 class UserAdmin(admin.ModelAdmin):
-    search_fields = ['username', 'points', 'get_real_cash', 'wallet_id', 'first_name', 'last_name', 'email', ]
+    search_fields = ['username', 'points', 'wallet_id', 'first_name', 'last_name', 'email', ]
     list_display = ['username', 'points', 'get_real_cash', 'first_name', 'wallet_id', 'email', 'is_active', 'level',
                     'date_joined']
     list_filter = ['is_active', ]
