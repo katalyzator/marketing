0953d17a58a0d4a0d4d6e0b11094fbf50d05f049
Nurs <jackmovies01@gmail.com>
2019-01-06 17:23:16 +0600

    2019-01-06

--- main/admin.py
+++ main/admin.py
@@ -2,27 +2,65 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from import_export import resources, fields
+from import_export.admin import ImportExportModelAdmin
 
 from main.models import *
 from marketing import settings
+from import_export.widgets import ForeignKeyWidget
 
 # Register your models here.
 
 admin.site.site_header = 'Панель управления'
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserResource(resources.ModelResource):
+    parent = fields.Field(widget=ForeignKeyWidget(User, 'username'), attribute='get_parent')
+
+    class Meta:
+        model = User
+        exclude = (
+            'id',
+            'last_login',
+            'is_superuser',
+            'groups',
+            'user_permissions',
+            'is_staff',
+            'is_active',
+            'date_joined',
+            'level',
+            'email',
+            'related_users'
+        )
+
+    # def clean_parent(self, user):
+    #     return user.get_parent
+
+
+# def dehydrate_parent(self, user):
+#
+#     try:
+#         return User.objects.get(related_users=user)
+#     except:
+#         return None
+# if user and user.get_parent:
+#     return user.get_parent
+# return None
+
+
+class UserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
     search_fields = ['username', 'points', 'wallet_id', 'first_name', 'last_name', 'email', ]
     list_display = ['username', 'points', 'get_real_cash', 'level', 'first_name', 'wallet_id', 'email', 'is_active',
                     'level',
                     'date_joined']
     list_filter = ['level', 'is_active', ]
 
-    class Media:
-        js = [
-            'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
-            settings.STATIC_URL + 'admin/js/export_to_excel.js'
-        ]
+    # class Media:
+    #     js = [
+    #         'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+    #         settings.STATIC_URL + 'admin/js/export_to_excel.js'
+    #     ]
 
     def get_real_cash(self, obj):
         payments = Payments.objects.filter(user=obj).aggregate(sum=Sum('sum', output_field=models.DecimalField()))[
